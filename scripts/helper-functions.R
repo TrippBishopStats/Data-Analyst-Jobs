d336e46ff322b42210bb16f9fcde6f4306d2bf06
TrippBishopStats <dyne_erg@yahoo.com>
2024-01-07 21:02:23 -0700

    inital commit of renv.
    Mostly polishing presentation and doing a fair bit of reorganisation of content to put it into a more logical order.

--- scripts/helper-functions.R
+++ scripts/helper-functions.R
@@ -1,3 +1,13 @@
 compute_percentage <- function(field,total) {
   round(sum(field)/total*100,1)
 }
+
+compute_p <- function(model) {
+  f_stat <- summary(model)$fstatistic
+  p_value <- pf(f_stat[1],
+                f_stat[2],
+                f_stat[3],
+                lower.tail=FALSE)
+  attributes(p_value) <- NULL
+  p_value
+}
